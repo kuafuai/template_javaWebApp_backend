0ec7ae788fb27137d50cde7027e5b7c46ef4fbf7
charging-kuafuai <charging@126.com>
2023-10-05 11:18:51 +0800

    支持附件管理功能 - 支持附件管理功能（增删改查），附件保存在服务器的/data目录中，通过附件id可以查询到相关信息

--- src/main/java/com/aiassistant/service/impl/AttachmentServiceImpl.java
+++ src/main/java/com/aiassistant/service/impl/AttachmentServiceImpl.java
@@ -1,7 +1,8 @@
-package com.aiassistant.service;
+package com.aiassistant.service.impl;
 
 import com.aiassistant.mapper.AttachmentMapper;
 import com.aiassistant.model.Attachment;
+import com.aiassistant.service.AttachmentService;
 import com.aiassistant.utils.ResultModel;
 import com.aiassistant.utils.ResultPageModel;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,36 +10,42 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
-public class AttachmentService {
+public class AttachmentServiceImpl implements AttachmentService {
+
     private final AttachmentMapper attachmentMapper;
 
     @Autowired
-    public AttachmentService(AttachmentMapper attachmentMapper) {
+    public AttachmentServiceImpl(AttachmentMapper attachmentMapper) {
         this.attachmentMapper = attachmentMapper;
     }
 
+    @Override
     public ResultModel<Attachment> addAttachment(MultipartFile file) {
-        // TODO: Implement this method
+        // TODO: Implement the logic to add an attachment
         return null;
     }
 
+    @Override
     public ResultModel<Attachment> getAttachmentById(Integer id) {
-        // TODO: Implement this method
+        // TODO: Implement the logic to get an attachment by id
         return null;
     }
 
+    @Override
     public ResultPageModel<Attachment> getAllAttachments() {
-        // TODO: Implement this method
+        // TODO: Implement the logic to get all attachments
         return null;
     }
 
+    @Override
     public ResultModel<Attachment> updateAttachment(Integer id, MultipartFile file) {
-        // TODO: Implement this method
+        // TODO: Implement the logic to update an attachment
         return null;
     }
 
+    @Override
     public ResultModel<Attachment> deleteAttachment(Integer id) {
-        // TODO: Implement this method
+        // TODO: Implement the logic to delete an attachment
         return null;
     }
 }
