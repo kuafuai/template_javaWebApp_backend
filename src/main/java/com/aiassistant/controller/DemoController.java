0eb3be277cfc7ad7882e74ec7a7a30768fe79ef6
charging-kuafuai <charging@126.com>
2023-10-24 17:47:24 +0800

    连锁门店管理系统，需要对接门店端，收集门店端的反馈数据，照片，视频，收集门店的销售数据、客户反馈和员工表现等信息，生成表格 并且借助系统给门店下发文件或通知，并进行门店后续的一个培训

--- src/main/java/com/aiassistant/controller/DemoController.java
+++ src/main/java/com/aiassistant/controller/DemoController.java
@@ -12,9 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * 控制器层--DemoController
- */
 @RestController
 @RequiredArgsConstructor
 @Api(tags = "DemoController")
