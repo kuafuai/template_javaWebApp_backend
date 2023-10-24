7ab41dfc0cd72eaee613dc59e21553ec71a59bd6
charging-kuafuai <charging@126.com>
2023-10-24 16:55:19 +0800

    连锁门店管理系统，需要对接门店端，收集门店端的反馈数据，照片，视频，收集门店的销售数据、客户反馈和员工表现等信息，生成excel表格 并且借助系统给门店下发文件或通知，并进行门店后续的一个培训

--- src/main/java/com/example/util/ExcelUtil.java
+++ src/main/java/com/example/util/ExcelUtil.java
@@ -7,12 +7,18 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 public class ExcelUtil {
 
-    public void generateExcel(List<Store> stores) {
+    private static final String DEFAULT_FILE_NAME = "store_data.xlsx";
+    private static final String DEFAULT_SHEET_NAME = "Store Data";
+
+    public void generateExcel(List<Store> stores, String fileName, String sheetName) {
+        validateInputParameters(fileName, sheetName);
+
         Workbook workbook = new XSSFWorkbook();
-        Sheet sheet = workbook.createSheet("Store Data");
+        Sheet sheet = workbook.createSheet(sheetName);
 
         // Create header row
         Row headerRow = sheet.createRow(0);
@@ -37,17 +43,35 @@ public class ExcelUtil {
         }
 
         // Write the workbook to a file
-        try (FileOutputStream fileOut = new FileOutputStream("store_data.xlsx")) {
+        try (FileOutputStream fileOut = new FileOutputStream(fileName)) {
             workbook.write(fileOut);
         } catch (IOException e) {
-            e.printStackTrace();
+            handleException("Error occurred while writing the Excel file: " + e.getMessage());
+        } finally {
+            // Close the workbook
+            try {
+                workbook.close();
+            } catch (IOException e) {
+                handleException("Error occurred while closing the workbook: " + e.getMessage());
+            }
         }
+    }
 
-        // Close the workbook
-        try {
-            workbook.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void generateExcel(List<Store> stores) {
+        generateExcel(stores, DEFAULT_FILE_NAME, DEFAULT_SHEET_NAME);
+    }
+
+    private void validateInputParameters(String fileName, String sheetName) {
+        if (Objects.isNull(fileName) || fileName.isEmpty()) {
+            throw new IllegalArgumentException("File name cannot be null or empty");
         }
+        if (Objects.isNull(sheetName) || sheetName.isEmpty()) {
+            throw new IllegalArgumentException("Sheet name cannot be null or empty");
+        }
+    }
+
+    private void handleException(String errorMessage) {
+        // Log the exception and return a custom error message to the user
+        throw new RuntimeException(errorMessage);
     }
 }
