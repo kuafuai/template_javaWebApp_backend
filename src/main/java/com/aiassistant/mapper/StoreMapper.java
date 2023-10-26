a05279892ed358eceef8b8168423e06bb473f0b5
charging-kuafuai <charging@126.com>
2023-10-26 16:17:04 +0800

    门店管理，查询所有门店。

--- src/main/java/com/aiassistant/mapper/StoreMapper.java
+++ src/main/java/com/aiassistant/mapper/StoreMapper.java
@@ -4,10 +4,16 @@ import com.aiassistant.model.Store;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface StoreMapper {
     @Insert("INSERT INTO store (name,address,timestamp) VALUES (#{name}, #{address}, #{timestamp})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void insertStore(Store store);
+
+    @Select("SELECT * FROM store")
+    List<Store> selectAllStores();
 }
--- src/main/java/com/aiassistant/service/StoreService.java
+++ src/main/java/com/aiassistant/service/StoreService.java
@@ -2,7 +2,9 @@ package com.aiassistant.service;
 
 import com.aiassistant.utils.ResultModel;
 import com.aiassistant.model.Store;
+import com.aiassistant.utils.ResultPageModel;
 
 public interface StoreService {
     ResultModel<Store> addStore(Store store);
+    ResultPageModel<Store> getAllStores(Integer pageNo, Integer pageSize);
 }
