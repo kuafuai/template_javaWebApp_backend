a5917e1e5d5a2951829036cd0054449a189fdb23
yake <jiangfei@kuafuai.net>
2023-10-27 09:58:19 +0800

    PMF 验证

--- src/main/java/com/aiassistant/mapper/FeedbackMapper.java
+++ src/main/java/com/aiassistant/mapper/FeedbackMapper.java
@@ -13,7 +13,7 @@ public interface FeedbackMapper {
     @Insert("INSERT INTO feedback (store_name,feedback_text, image_url, created_at) VALUES (#{storeName},#{feedbackText},#{imageUrl},#{createdAt})")
     void insertFeedback(Feedback feedback);
 
-    @Select("select * from feedback")
+    @Select("select * from feedback order by id desc")
     List<Feedback> getFeedbackList();
 
     @Select("select * from feedback where id = #{id}")
