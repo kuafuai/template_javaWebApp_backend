01a725c0f3ff79c29a2964d1389721f7ca3235c9
yake <jiangfei@kuafuai.net>
2023-10-27 15:04:14 +0800

    PMF 验证

--- src/main/java/com/aiassistant/config/StaticResourceConfig.java
+++ src/main/java/com/aiassistant/config/StaticResourceConfig.java
@@ -10,6 +10,6 @@ public class StaticResourceConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/custom-resources/**") // 自定义资源路径
-                .addResourceLocations("file:/data/app/html"); // 静态资源文件夹的位置
+                .addResourceLocations("file:/data/app/htm"); // 静态资源文件夹的位置
     }
 }
