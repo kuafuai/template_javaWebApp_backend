84b555f3fba19528705fc501e25de09f1a9bb016
charging-kuafuai <charging@126.com>
2023-11-05 21:10:05 +0800

    资产登记功能：支持添加新的固定资产信息，包括资产编号、名称、规格、采购日期、采购价格。

--- src/main/java/com/aiassistant/service/impl/AssetServiceImpl.java
+++ src/main/java/com/aiassistant/service/impl/AssetServiceImpl.java
@@ -1,1 +1,47 @@
-在com.aiassistant.service.impl包中定义AssetServiceImpl类
+package com.aiassistant.service.impl;
+
+import com.aiassistant.mapper.AssetMapper;
+import com.aiassistant.model.Asset;
+import com.aiassistant.service.AssetService;
+import com.aiassistant.utils.ResultModel;
+import com.aiassistant.utils.ResultPageModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AssetServiceImpl implements AssetService {
+
+    private final AssetMapper assetMapper;
+
+    @Autowired
+    public AssetServiceImpl(AssetMapper assetMapper) {
+        this.assetMapper = assetMapper;
+    }
+
+    @Override
+    public ResultModel<Asset> addAsset(Asset asset) {
+        try {
+            assetMapper.insertAsset(asset);
+            return ResultModel.ofSuccess(asset);
+        } catch (Exception e) {
+            return ResultModel.ofError("Failed to add asset", e);
+        }
+    }
+
+    @Override
+    public ResultPageModel<Asset> getAssetList() {
+        try {
+            ResultPageModel<Asset> resultPageModel = new ResultPageModel<>();
+            resultPageModel.setList(assetMapper.getAssetList());
+            resultPageModel.setTotalRecords(resultPageModel.getList().size());
+            return resultPageModel;
+        } catch (Exception e) {
+            return ResultPageModel.ofResult(0, "Failed to get asset list", null, e);
+        }
+    }
+
+    @Override
+    public Asset getById(Integer id) {
+        return assetMapper.selectById(id);
+    }
+}
