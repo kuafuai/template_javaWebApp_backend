82239f95d56964ba82c90b405540c9ce390e5fff
charging-kuafuai <charging@126.com>
2024-02-27 18:05:51 +0800

    发表文字博客，博客字数限制200字

--- src/main/java/com/aiassistant/utils/ResultModel.java
+++ src/main/java/com/aiassistant/utils/ResultModel.java
@@ -14,23 +14,23 @@ public class ResultModel<T> {
         this.data = data;
     }
 
-    public static <T> ResultModel<T> ofResult(Integer code, String msg, T data) {
+    public static ResultModel<String> ofResult(Integer code, String msg, String data) {
         return new ResultModel<>(code, msg, data);
     }
 
-    public static <T> ResultModel<T> ofSuccess(T data) {
+    public static ResultModel<String> ofSuccess(String data) {
         return new ResultModel<>(200, "success", data);
     }
 
-    public static <T> ResultModel<T> ofError(String msg) {
+    public static ResultModel<String> ofError(String msg) {
         return new ResultModel<>(500, msg, null);
     }
 
-    public T getData() {
+    public String getData() {
         return data;
     }
 
-    public void setData(T data) {
+    public void setData(String data) {
         this.data = data;
     }
 }
