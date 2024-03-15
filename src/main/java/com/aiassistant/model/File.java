1e5a1c89b6fd9b09168223e96400f9221e74933f
charging-kuafuai <charging@126.com>
2024-03-15 14:15:34 +0800

    开发一个上传文件接口，将上传的文件保存到服务器中，并将文件信息：如文件名、文件类型、文件大小保存到数据库

--- src/main/java/com/aiassistant/model/File.java
+++ src/main/java/com/aiassistant/model/File.java
@@ -1,1 +1,40 @@
-在`com.aiassistant.model.File`文件中定义`File`类
+package com.aiassistant.model;
+
+public class File {
+    private int id;
+    private String fileName;
+    private String fileType;
+    private long fileSize;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+}
