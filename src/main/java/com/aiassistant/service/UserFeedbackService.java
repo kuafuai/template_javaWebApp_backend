b564da6d65dc752716236e0e2cb6558ac6d22427
charging-kuafuai <charging@126.com>
2024-07-21 12:35:21 +0800

    我想创建一个待办清单管理工具，这个工具融入了游戏化元素。用户会有多个清单池，并对应不同的奖励。这些奖励可以用于用户升级和道具兑换等。

--- src/main/java/com/aiassistant/service/UserFeedbackService.java
+++ src/main/java/com/aiassistant/service/UserFeedbackService.java
@@ -1,1 +1,11 @@
-设计用户反馈和评价机制接口
+package com.aiassistant.service;
+
+import com.aiassistant.utils.ResultModel;
+import com.aiassistant.utils.ResultPageModel;
+import com.aiassistant.model.UserFeedback;
+
+public interface UserFeedbackService {
+    ResultModel<UserFeedback> submitFeedback(UserFeedback feedback);
+    ResultPageModel<UserFeedback> getFeedbackList(int pageNo, int pageSize);
+    ResultModel<UserFeedback> getFeedbackById(Integer feedbackId);
+}
