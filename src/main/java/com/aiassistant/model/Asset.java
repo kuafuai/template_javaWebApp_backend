4645a7dc6876d417053199123b81dea5b5aab94d
charging-kuafuai <charging@126.com>
2024-08-01 16:02:19 +0800

    资产登记功能：支持添加新的固定资产信息，包括资产编号、名称、规格、采购日期、采购价格。

--- src/main/java/com/aiassistant/model/Asset.java
+++ src/main/java/com/aiassistant/model/Asset.java
@@ -1,1 +1,15 @@
-Define Asset Entity Class
+package com.aiassistant.model;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Asset {
+    private String assetId;
+    private String name;
+    private String specifications;
+    private Date purchaseDate;
+    private Float purchasePrice;
+    private String assetCategory;
+}
--- src/main/java/com/aiassistant/service/AssetService.java
+++ src/main/java/com/aiassistant/service/AssetService.java
@@ -1,1 +1,8 @@
-Define Asset Service Interface
+package com.aiassistant.service;
+
+import com.aiassistant.utils.ResultModel;
+import com.aiassistant.model.Asset;
+
+public interface AssetService {
+    ResultModel addAsset(Asset asset);
+}
