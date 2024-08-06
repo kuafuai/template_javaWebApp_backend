1ae7fb3d2d210ab1d13bdadda33182921df0bc96
charging-kuafuai <charging@126.com>
2024-08-06 11:32:44 +0800

    抽签页面开发，用户提前导入人员名单。每轮抽奖中，用户输入中奖人数，随机抽签。对应展示中奖人员名单

--- src/main/java/com/aiassistant/model/LotteryEntry.java
+++ src/main/java/com/aiassistant/model/LotteryEntry.java
@@ -4,6 +4,6 @@ import lombok.Data;
 
 @Data
 public class LotteryEntry {
-    private String name;        // 中奖人员姓名
-    private String employeeId;  // 中奖人员工号
+    private String name;
+    private String employeeId;
 }
